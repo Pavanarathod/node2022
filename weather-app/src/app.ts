882a7da5d3570c60fe7738cd35cfe4e01d2e91bd
Pavanarathod <edrocullen13@gmail.com>
2022-08-08 22:14:31 +0530

    some issue with ts node

--- weather-app/src/app.ts
+++ weather-app/src/app.ts
@@ -3,10 +3,13 @@ import fetch from "node-fetch";
 
 const get = async () => {
   const data = await fetch(
-    `${API}?access_key=${USER_KEY}&query=12.120000, 76.680000`
+    `${API}?access_key=${USER_KEY}&query=13.929930,75.568100&units=f`
   );
   const result = await data.json();
   console.log(result);
+  console.log(
+    `It is currently ${result.current.temperature} degrees out. There is ${result.current.feelslike}% chance of rain.`
+  );
 };
 
 get();
